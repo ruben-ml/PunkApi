2d1a4d7530c82f59d43b628251e36c699e5e2e40
Rubén Muñoz López <ruben.munoz@ciberexperis.es>
2022-05-09 10:09:36 +0200

    Added title to view controller

--- PunkApi/PunkApi/ViewController.swift
+++ PunkApi/PunkApi/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Beers list"
+        label.textColor = .black
+        label.textAlignment = .center
+        label.font = UIFont.init(name: "HelveticaNeue-Bold", size: 25)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .orange
+        view.addSubview(titleLabel)
+        setupNavigationUI()
     }
 
+    private func setupNavigationUI() {
 
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
+    }
 }
 
