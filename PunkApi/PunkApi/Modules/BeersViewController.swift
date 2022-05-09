ba74044b206e381ee2d514068c1ba127c472edf7
Rubén Muñoz López <ruben.munoz@ciberexperis.es>
2022-05-09 17:08:01 +0200

    Added title label

--- PunkApi/PunkApi/Modules/BeersViewController.swift
+++ PunkApi/PunkApi/Modules/BeersViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 import Moya
 
 class BeersViewController: UIViewController {
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = LocalizableKey.titleLabel.localizedCapitalized
+        label.textColor = .black
+        label.textAlignment = .center
+        label.font = UIFont.init(name: "HelveticaNeue-Bold", size: 25)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     var presenter: BeersPresenter?
     
@@ -25,6 +35,17 @@ class BeersViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .green
         presenter?.getBeers()
+        view.addSubview(titleLabel)
+        setupNavigationUI()
+    }
+
+    private func setupNavigationUI() {
+
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
     }
 
 }
---
+++ PunkApi/PunkApi/Utils/LocalizableKey.swift
@@ -0,0 +1,12 @@
+//
+//  LocalizableKey.swift
+//  PunkApi
+//
+//  Created by Rubén Muñoz López on 9/5/22.
+//
+
+import Foundation
+
+struct LocalizableKey {
+    static let titleLabel = "title_label"
+}
