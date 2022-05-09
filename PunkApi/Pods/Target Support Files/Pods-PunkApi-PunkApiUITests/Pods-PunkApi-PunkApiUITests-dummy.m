f9e2567b0a37a8e08f50809e66e73d451fdbf9bf
Rubén Muñoz López <ruben.munoz@ciberexperis.es>
2022-05-09 10:41:35 +0200

    Install dependencies

---
+++ PunkApi/Pods/Target Support Files/Pods-PunkApi-PunkApiUITests/Pods-PunkApi-PunkApiUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PunkApi_PunkApiUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PunkApi_PunkApiUITests
+@end
---
+++ PunkApi/Pods/Target Support Files/Pods-PunkApi/Pods-PunkApi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PunkApi : NSObject
+@end
+@implementation PodsDummy_Pods_PunkApi
+@end
